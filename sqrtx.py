101e12633108a3af6d07bea75f5fc644f7d2d3f9
nowang6 <150778393+nowang6@users.noreply.github.com>
2024-02-26 20:20:07 +0800

    Create sqrtx.py

---
+++ sqrtx.py
@@ -0,0 +1,18 @@
+#https://leetcode.cn/problems/sqrtx/
+
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        l, r, res = 0, x, -1
+        while l < r:
+            mid = (l+r) // 2
+            if mid * mid <= x:
+                res = mid
+                l = mid + 1
+            else:
+                r = mid - 1
+        return res
+
+
+s = Solution()
+res = s.mySqrt(8)
+print(res)
